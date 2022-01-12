22ff39e525e97a83ac49da87727ef373f8d9118e
Joachim Wehner <j.wehner@telekom.de>
2022-01-12 15:10:56 +0100

    Start 20220112

--- src/main/java/de/telekom/sea7/BierVorlage.java
+++ src/main/java/de/telekom/sea7/BierVorlage.java
@@ -1,34 +1,56 @@
 package de.telekom.sea7;
 
 public class BierVorlage {
+	// aus Bier.java wurde BierVorlage.java (Modus static überall entfernt, um
+	// Instanzen zu bilden)
+	// die BierVorlage kann man auch als Vorlage oder Protoyp für Inststanzen
+	// Objekte verstehen
+	// hier den cast-Operator verwendet - siehe
+	// https://www.java-tutorial.org/typecasting.html - , um int in float und zurück
+	// zu wandeln.
+	// Berechnung leicht optimiert
+	// Ist mit Verlust der Nachkommastellen verbunden
 
 	private int preis = 200; // 400
 	private int netto = 186; // ?
 	private int steuer = 14; // ?
 	private int steuersatz = 7; // Steuersatz in % /100
+	private String name = "Bock";
 
 	public void preisanpassen(int preisanpassung) {
-	preis = preis * preisanpassung;
-	preisrechnen();
+		preis = preis * preisanpassung;
+		preisrechnen();
 	}
 
 	private void preisrechnen() {
-	//	System.out.println("preis+ " + (1 - ((float) steuersatz / 100)));
-	netto = (int) (preis * (1 - ((float) steuersatz / 100))); // 7 / 100 = 0 .07
-	//  System.out.println("netto: " + netto);
-	steuer = preis - netto;
+		// System.out.println("preis+ " + (1 - ((float) steuersatz / 100)));
+		netto = (int) (preis * (1 - ((float) steuersatz / 100))); // 7 / 100 = 0 .07
+		// System.out.println("netto: " + netto);
+		steuer = preis - netto;
 	}
 
 	public int getPreis() {
-	return preis;
+		return preis;
 	}
+
 	public int getSteuer() {
-	return steuer;
+		return steuer;
 	}
 
 	public void setPreis(int i) {
-	preis = i;
-	preisrechnen();
+		preis = i;
+		preisrechnen();
+	}
+
+	public void setName(String nameArg) {
+		// TODO Auto-generated method stub
+		name = nameArg;
+		
+	}
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return name;
 	}
 
 }
--- src/main/java/de/telekom/sea7/LadenPlus.java
+++ src/main/java/de/telekom/sea7/LadenPlus.java
@@ -16,14 +16,31 @@ public class LadenPlus {
 		String ort = "Hamburg"; // Laden liegt in
 
 		altbier.setPreis(75);
+		altbier.setName("Alt");
+		altbier.getName();
 
 		altbier.preisanpassen(2); // Preisverdopplung
+		
+		bockbier.setPreis(400);
+		bockbier.setName("Bock");
+		
+		BierVorlage[] unsereBiere = new BierVorlage[2];
+		unsereBiere[0] = altbier;
+		unsereBiere[1] = bockbier;
+		
+		for(int i = 0; i < unsereBiere.length ;i++){
+			float preis = (float) unsereBiere[i].getPreis() / 100;
+			//String name = unsereBiere[i].getName();
+			String ausgabeForm = "Wir haben die Sorte %s, mit einem Preis von %.2f €";
+			System.out.println(String.format(ausgabeForm, unsereBiere[i].getName(), preis));
+		}
 
 // Bier.getPreis() = 75;
 
 		System.out.println("Verkauf in " + ort);
 		System.out.println("Verkaufspreis  in Cent ist " + altbier.getPreis());
 		System.out.println("Steuer  in Cent bei " + altbier.getSteuer());
+		
 
 	}
 
